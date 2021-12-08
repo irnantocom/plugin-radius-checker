91c8d3815a7efd912172806fcef53083ed9ae5e7
irnanto <irnanto@tonjoo.com>
2021-12-08 20:11:17 +0700

    research filter kml

--- assets/js/front.js
+++ assets/js/front.js
@@ -6,15 +6,46 @@ function initMap() {
 
   if (mapdata) {
     const kmlLayer = new google.maps.KmlLayer({
-      url: mapdata.kml,//"https://raw.githubusercontent.com/googlearchive/kml-samples/gh-pages/kml/Placemark/placemark.kml",
+      url: mapdata.kml,
       suppressInfoWindows: true,
       map: map,
     });
 
-    kmlLayer.addListener("click", (kmlEvent) => {
+    // kmlLayer.addListener("click", (kmlEvent) => {
+    //   // const text = kmlEvent.featureData.description;
+    //   console.log(kmlEvent.featureData)
+    // });
+    // 
+    kmlLayer.addListener("tilesloaded", (kmlEvent) => {
       // const text = kmlEvent.featureData.description;
       console.log(kmlEvent.featureData)
     });
+    
+    // kmlLayer.addListener(search_address);
+
+    // kmlLayer.addEventListener('load', (event) => {
+    //     event.featureData.map(search_address)
+    //     // console.log('page is fully loaded');
+        
+    //     var BreakException = {};
+
+    //     try {
+    //       event.featureData.forEach(function(el) {
+    //         console.log(el);
+    //         if (el.name == "Melbourne CBD") throw BreakException;
+    //       });
+    //     } catch (e) {
+    //       if (e !== BreakException) throw e;
+    //     }
+    // });
+    
+
   }
 
+}
+
+function search_address(data) {
+  if (data.name == "Melbourne CBD") {
+    console.log('asd')
+  }
 }
